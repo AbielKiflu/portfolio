9808b511bf83d0a43accd65c2322528963b5a7a1
Abel <abiel.kiflu.weld@gmail.com>
2022-11-20 16:22:49 +0100

    Modification header

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Carusel from "../components/carusel/Carusel";
 
 const About = () => {
   return (
     <div>
       <h1 style={{ color: "white" }}>About</h1>
       <p style={{ color: "white" }}>This is a page</p>
-      <Carusel />
     </div>
   );
 };
