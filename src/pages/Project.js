eb5b6709014a7452c7e0e48429cea1719d3dfcee
Abel <abiel.kiflu.weld@gmail.com>
2022-11-29 19:59:51 +0100

    carousel

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,5 +1,24 @@
+import "./projects.css";
+import React from "react";
+
 const Project = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="scene">
+        <div className="carousel">
+          <div className="carousel__card">A</div>
+          <div className="carousel__card">B</div>
+          <div className="carousel__card">C</div>
+          <div className="carousel__card">D</div>
+          <div className="carousel__card">F</div>
+          <div className="carousel__card">G</div>
+          <div className="carousel__card">H</div>
+          <div className="carousel__card">I</div>
+          <div className="carousel__card">J</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
