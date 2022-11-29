39e7a12a2e10d39cdd652d56459de8a731736eea
Abel <abiel.kiflu.weld@gmail.com>
2022-11-29 17:51:45 +0100

    skill cards

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,8 +1,42 @@
 import React from "react";
+import {
+  FaHtml5,
+  FaSass,
+  FaReact,
+  FaPhp,
+  FaVuejs,
+  FaCss3Alt,
+  FaBootstrap,
+  FaLinux,
+} from "react-icons/fa";
 import "./skills.css";
 
 const Skills = () => {
-  return <div className="container">Hi</div>;
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-icon">
+          <FaSass className="icon" />
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-icon">
+          <FaSass className="icon" />
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-icon">
+          <FaSass className="icon" />
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-icon">
+          <FaSass className="icon" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
