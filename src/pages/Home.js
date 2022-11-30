2f4b5c3bbc60c0be954d3692bef39db92195aec6
Abel <abiel.kiflu.weld@gmail.com>
2022-11-30 23:15:14 +0100

    background color

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,45 @@ import {
   FaHome,
   FaGraduationCap,
   FaFileCode,
+  FaUserAlt,
   FaRegAddressBook,
 } from "react-icons/fa";
+import Skills from "./Skills";
 
 const Home = () => {
+  const icons = { width: "2rem", height: "2rem" };
   return (
-    <section id="home" className="home">
-      <nav className="nav">
-        <FaHome style={{ width: "2rem", height: "2rem" }} />
-        <FaGraduationCap style={{ width: "2rem", height: "2rem" }} />
-        <FaRegAddressBook style={{ width: "2rem", height: "2rem" }} />
-        <FaFileCode style={{ width: "2rem", height: "2rem" }} />
-      </nav>
-    </section>
+    <>
+      <section id="home" className="home">
+        <nav className="nav">
+          <a href="#home">
+            <FaHome style={icons} />
+            <span>Home</span>
+          </a>
+
+          <a href="#about">
+            <FaUserAlt style={icons} />
+            <span>About</span>
+          </a>
+
+          <a href="#skill">
+            <FaGraduationCap style={icons} />
+            <span>Skills</span>
+          </a>
+          <a href="#contact">
+            <FaFileCode style={icons} />
+            <span>Contact</span>
+          </a>
+          <a href="#project">
+            <FaRegAddressBook style={icons} />
+            <span>Project</span>
+          </a>
+        </nav>
+      </section>
+      <section id="skill" className="skill">
+        <Skills />
+      </section>
+    </>
   );
 };
 
